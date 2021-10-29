6b24f8d9fedf2d5b1d1b1b8f439263246380b62c
Sbastdia <sbastdia@myuax.com>
2021-10-29 16:05:29 +0200

    clase terrícola

--- claseTerrícola.py
+++ claseTerrícola.py
@@ -19,7 +19,42 @@ class Terricola(Guerrero):
 
         if(self._vivo):
             shot = generaIntAleatorio(0,Guerrero.get_maxTarget())
-        while(Terricola.__shots_done[shot] >=
-            Marciano.get_shotsToKillAMarciano()):
-            shot = generaIntAleatorio(0,Guerrero.get_maxTarget())
-            Terricola.__shots_done[shot] += 1
+            while(Terricola.__shots_done[shot] >=
+                Marciano.get_shotsToKillAMarciano()):
+                shot = generaIntAleatorio(0,Guerrero.get_maxTarget())
+                Terricola.__shots_done[shot] += 1
+
+                print(self._name + " shoot " + str(shot) + " for " +
+                str(Terricola.__shots_done[shot]) + " time")
+
+                return shot
+        else:
+            return -1
+# Overrides the method get_shot from the parent class!
+    def get_shot(self, shot):
+
+#  If the target is guessed by the shoot, then the warrior dies.
+#  :param shoot: int with the shoot against the soldier
+#  :returns True if the shot kills the warrior (shot is the target and
+# the warrior is alive), False otherwise
+
+        isTarget = Guerrero.get_shot(self,shot)
+        if(self._vivo):
+            __totalTerricolasAlive -= 1
+            return isTarget
+    @staticmethod
+    def get_total_Terricolas_alive():
+
+#  :returns the total number of Terricolas alive
+
+        return Terricola.__totalTerricolasAlive
+    @staticmethod
+    def get_total_shots_done(shoot):
+
+# :returns the total number of Terricolas alive
+
+        if(not isinstance(shoot,int)):
+            raise TypeError("shoot should be an int")
+        elif(shoot < 0 or shoot > Guerrero.get_maxTarget()):
+            raise ValueError("shoot OUT OF RANGE")
+            return Terricola.__shots_done[shoot]
